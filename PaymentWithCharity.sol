3af0eae2235fa1e61d6cf7baaded707e213706fb
Victor Del Angel <videla@microsoft.com>
2021-10-14 15:26:35 -0500

    first

---
+++ PaymentWithCharity.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.8;
+
+contract PaymentWithCharity{
+
+    uint buyer = 100;
+    uint seller = 0;
+    uint charity= 0;
+
+    function get() returns (uint){
+
+        return buyer;
+
+    }
+
+    function set(uint sellerAmount, uint charityAmount){
+
+        seller += sellerAmount;
+        charity += charityAmount;
+
+        buyer = buyer - (sellerAmount + charityAmount);
+
+    }
+
+
+}
+
