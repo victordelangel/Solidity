b83c9a53276506f200ddc7ecfd5f4903634a78ba
Victor Del Angel <videla@microsoft.com>
2021-10-14 13:03:46 -0500

    first

--- mysmartcontract.sol
+++ mysmartcontract.sol
@@ -1,6 +1,12 @@
-pragma solidity >=0.4.9;
+pragma solidity ^0.5.13;
 
 contract MyContract{
 
     string public myString = "Hello World";
+
+    function setMessage(string _message) public {
+
+        myString = _message;
+    }
+
 }
